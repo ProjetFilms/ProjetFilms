54283e6df173dff0fd96d080b08a9136b15d8087
Lucas <lucas.hamrioui@gmail.com>
2023-11-22 16:25:57 +0100

    ajout

---
+++ src/app/films/films-list/films-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-films-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './films-list.component.html',
+  styleUrl: './films-list.component.css'
+})
+export class FilmsListComponent {
+
+}
