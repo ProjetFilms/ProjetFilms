10d873b076f8c28807a9cfa1b2562fb7bbb139a8
Lucas <lucas.hamrioui@gmail.com>
2023-12-06 10:31:11 +0100

    TEST

--- src/app/films/films-list/films-list.component.ts
+++ src/app/films/films-list/films-list.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Film } from '../films.model';
 
 @Component({
   selector: 'app-films-list',
   templateUrl: './films-list.component.html',
   styleUrl: './films-list.component.css',
 })
-export class FilmsListComponent {
+
+export class FilmsListComponent implements OnInit {
+  listFilms: Array<Film> = new Array<Film>();
   profileForm = this.fb.group({
     Name: ['FightClub', [Validators.required, Validators.minLength(4)]],
     Real: [''],
@@ -32,5 +36,7 @@ export class FilmsListComponent {
 
   ngOnInit() {
     this.profileForm.patchValue(this.data);
+    this.listFilms.push(new Film("Avenger"))
+    this.listFilms.push(new Film("zdaha"))
   }
 }
---
+++ src/app/films/films.model.ts
@@ -0,0 +1,9 @@
+export class Film {
+    titre: string;
+    realisateur: string;
+
+    constructor(titre: string = ''){
+        this.titre = titre;
+        this.realisateur = "";
+    }
+}
