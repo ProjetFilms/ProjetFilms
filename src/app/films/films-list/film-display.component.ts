0a30deef851a95a543e817ea3467dd356c0a3873
Lucas <lucas.hamrioui@gmail.com>
2023-12-06 14:49:35 +0100

    hgiu

--- src/app/films/films-list/film-display.component.ts
+++ src/app/films/films-list/film-display.component.ts
@@ -1,3 +1,47 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core"
-import { FormBuilder } from "@angular/forms"
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Film } from '../films.model';
+
+@Component({
+ selector: 'app-film-form',
+ templateUrl: './film-list.component.html',
+ styleUrls: ['./film-list.component.css']
+})
+export class FilmsListComponent {
+
+    @Input()
+    model: Film | null = null;
+    @Output()
+    emitFilm: EventEmitter<Film> = new EventEmitter<Film>();
+
+    filmform = this.fb.group({
+    titre: this.fb.control('', [Validators.required]),
+    realisateur: this.fb.control(0),
+    }, { updateOn: 'submit' });
+    submitted = false;
+    constructor(private fb: FormBuilder) { }
+
+    ngOnInit() {
+    this.submitted = false;
+ 
+    if (this.model === null) {
+    this.model = new Film();
+    } else {
+    this.filmform.patchValue(this.model);
+    }
+    }
+    onSubmit() {
+    
+    this.submitted = true;
+    if (this.filmform.valid) {
+    this.model = {...this.model!, ...this.filmform.value };
+        this.emitFilm.emit(this.model!);
+    }
+    }
+    resetForm() {
+    if (this.model !== null) {
+    this.filmform.patchValue(this.model);
+    }
+    }
+}
 
