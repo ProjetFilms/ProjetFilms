fce012065f48d49bfd6103361492ac9a0dfb8f3c
Ethan CHARLIER <ethan.charlier@gmail.com>
2023-12-06 15:21:49 +0100

    change model

--- src/app/films/films-list/film-display.component.ts
+++ src/app/films/films-list/film-display.component.ts
@@ -3,45 +3,45 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Film } from '../films.model';
 
 @Component({
- selector: 'app-film-form',
- templateUrl: './film-list.component.html',
- styleUrls: ['./film-list.component.css']
+  selector: 'app-film-form',
+  templateUrl: './film-list.component.html',
+  styleUrls: ['./film-list.component.css'],
 })
 export class FilmsListComponent {
+  @Input()
+  model: Film | null = null;
+  @Output()
+  emitFilm: EventEmitter<Film> = new EventEmitter<Film>();
 
-    @Input()
-    model: Film | null = null;
-    @Output()
-    emitFilm: EventEmitter<Film> = new EventEmitter<Film>();
+  filmform = this.fb.group(
+    {
+      titre: this.fb.control('', [Validators.required]),
+      realisateur: this.fb.control(''),
+    },
+    { updateOn: 'submit' }
+  );
+  submitted = false;
+  constructor(private fb: FormBuilder) {}
 
-    filmform = this.fb.group({
-    titre: this.fb.control('', [Validators.required]),
-    realisateur: this.fb.control(0),
-    }, { updateOn: 'submit' });
-    submitted = false;
-    constructor(private fb: FormBuilder) { }
-
-    ngOnInit() {
+  ngOnInit() {
     this.submitted = false;
- 
+
     if (this.model === null) {
-    this.model = new Film();
+      this.model = new Film();
     } else {
-    this.filmform.patchValue(this.model);
+      this.filmform.patchValue(this.model);
     }
-    }
-    onSubmit() {
-    
+  }
+  onSubmit() {
     this.submitted = true;
     if (this.filmform.valid) {
-    this.model = {...this.model!, ...this.filmform.value };
-        this.emitFilm.emit(this.model!);
+      this.model = { ...this.model!, ...this.filmform.value };
+      this.emitFilm.emit(this.model!);
     }
-    }
-    resetForm() {
+  }
+  resetForm() {
     if (this.model !== null) {
-    this.filmform.patchValue(this.model);
+      this.filmform.patchValue(this.model);
     }
-    }
+  }
 }
-
