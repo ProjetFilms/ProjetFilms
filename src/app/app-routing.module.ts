2d52197caa37f8bf479a2be21158cbe28f620d81
Ethan CHARLIER <ethan.charlier@gmail.com>
2023-12-06 16:24:25 +0100

    add file and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,4 +4,15 @@ import { FilmsListComponent } from './films/films-list/films-list.component';
 import { FilmDisplayComponent } from './films/film-display/film-display.component';
 //import { PageNotFoundComponent } from './404.component';
 
-const routes: Routes = [{ path: '', component: FilmDisplayComponent }];
+const routes: Routes = [
+  { path: 'film/edit/:id', component: FilmDisplayComponent },
+  { path: 'film/list', component: FilmsListComponent },
+  { path: '', redirectTo: 'film/list', pathMatch: 'full' },
+  //{ path:'**',component:PageNotFoundComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
